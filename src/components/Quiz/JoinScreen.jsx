33d4a33349825ff260d42fbf830d87949ed31f06
PolinaAleksandrova <polina.aleksandrova@nure.ua>
2023-02-20 04:28:31 +0100

    skip function in quiz fixed

--- src/components/Quiz/JoinScreen.jsx
+++ src/components/Quiz/JoinScreen.jsx
@@ -1,14 +1,13 @@
 import React from "react";
-
-function JoinScreen({start}){
-    return(
+function JoinScreen({ start }) {
+    return (
         <div className="join-screen">
-        <h2>Join screen</h2>
-        
-        <p>CLick on button below to start the Quiz.</p>
-        <button onClick={start}>start</button>
+            <h2>Join Quiz</h2>
+            <p>Press Start</p>
+            <button onClick={start}>Start</button>
         </div>
-    )
+
+    );
 }
 
 export default JoinScreen;
--- src/components/Quiz/QuizScreen.jsx
+++ src/components/Quiz/QuizScreen.jsx
@@ -1,51 +1,57 @@
-import React, { useState } from "react";
+import React from "react";
+import { useState } from "react";
 import QuestionList from "../Quiz/questions.json";
-import QuestionResult from "../Quiz/QuizResult";
 import Question from "../Quiz/Question";
+import QuizResult from "../Quiz/QuizResult";
+import "../../index.css"
 
-
-function QuizScreen({retry}){
-
-    const[currentQuestionIndex, setCurrentQuestionIndex] = useState(0);
-    const [markedAnswer, setMarkedAnswers] = useState(0);
+function QuizScreen({ retry }) {
+    const [currentQuestionIndex, setCurrentQuestionIndex] = useState(0);
+    const [markedAnswers, setMarkedAnswers] = useState(new Array(QuestionList.length));
     const isQuestionEnd = currentQuestionIndex === QuestionList.length;
 
-    function calculatedResult() {
-        console.log("marked",markedAnswer);
-        return{
-            total :QuestionList.length,
-            correct:markedAnswer,
-            percentage :Math.trunc((markedAnswer/ QuestionList.length)*100)
+    function calculateResult() {
+        console.log({ markedAnswers });
+        let correct = 0;
+        QuestionList.forEach((question, index) => {
+            if (question.correctOptionIndex == markedAnswers[index]) {
+                correct++;
+            }
+        });
+        return {
+            total: QuestionList.length,
+            correct: correct,
+            percentage: Math.round((correct / QuestionList.length) * 100)
         };
     }
 
-    function CheckAnswer(index) {
-        console.log("F ",index);
-        console.log(QuestionList[currentQuestionIndex].correctOptionIndex);
-        if ( index === QuestionList[currentQuestionIndex].correctOptionIndex) {
-            setMarkedAnswers(markedAnswer+1)
-        }
-        setCurrentQuestionIndex(currentQuestionIndex+1)
-    }
+    console.log(currentQuestionIndex)
 
-    return(
-        <div className="quiz-screen">
-        {
-            isQuestionEnd ? 
-                <QuestionResult 
-                    result={calculatedResult()}
-                    retry = {retry}
-                />
-             : 
-                <Question
-                 question={QuestionList[currentQuestionIndex]}
-                 totalQuestions = {QuestionList.length}
-                 currentQuestion = {currentQuestionIndex+1}
-                 setAnswer = {CheckAnswer} />
-
-        }
+    return (
+        <div classsName="quiz-screen">
+            {
+                isQuestionEnd ? (
+                    <QuizResult
+                        result={calculateResult()}
+                        retry={retry}
+                    />
+                ) : (
+                    <Question question={QuestionList[currentQuestionIndex]}
+                        totalQuestions={QuestionList.length} 
+                        currentQuestion={currentQuestionIndex}
+                        setAnswer={(index) => {
+                            setMarkedAnswers((arr) => {
+                                let newArr = [...arr];
+                                newArr[currentQuestionIndex] = index;
+                                return newArr;
+                            });
+                            setCurrentQuestionIndex(currentQuestionIndex + 1);
+                        }}
+                    />
+                )
+            }
         </div>
-    )
+    );
 }
 
 export default QuizScreen;
