5dbe6c4a120ec3af76cb15ce50c9f8abef2daac3
PolinaAleksandrova <polina.aleksandrova@nure.ua>
2023-02-27 00:47:30 +0100

    routing for mainCourses fixed

--- src/components/pages/mainCourses/MainCourses.js
+++ src/components/pages/mainCourses/MainCourses.js
@@ -1,21 +1,20 @@
 import React from 'react';
 import "../mainCourses/style.css";
-import "../mainCourses/las vegas.jpg"
-import "../mainCourses/newyork.jpg"
-import "../mainCourses/singapore.jpg"
-import { Component } from 'react';
-class MainCourses extends Component{
-render(){
+
+
+    function MainCourses(){
     return(
-        <div class="card">
-  <div class="card-content">
-    <h2 class="card-title">Something awsome</h2>
-    <p class="card-body">Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugait rem facilis.</p>
+        <div class="MainCourses">
+        <div class="cardMain">
+  <div class="cardMain-content">
+    <h2 class="cardMain-title">Something awsome</h2>
+    <p class="cardMain-body">Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugait rem facilis.</p>
     <a href="#" class="button">Learn More</a>
   </div>
 </div>
+</div>
     );
 }
-}
+
 
 export default MainCourses;
